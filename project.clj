68b769fdaa25e64491058a8d5ba9b3a44b78c2f9
Ivan Mikushin <i.mikushin@gmail.com>
2015-02-20 18:36:31 +0500

    bump clojurescript and lein-cljsbuild

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]]
+                 [org.clojure/clojurescript "0.0-2894"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]
+  :plugins [[lein-cljsbuild "1.0.5"]
             [lein-npm "0.5.0"]]
 
   :cljsbuild {
